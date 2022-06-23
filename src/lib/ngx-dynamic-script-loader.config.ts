bbbdbb19529fd44bfb03e5b25fafbb0e6e3cdb03
ekkolon <ekkolon@pubsink.org>
2022-06-23 12:35:45 +0200

    refactor: use one-line arrow function for noop fn

--- src/lib/ngx-dynamic-script-loader.config.ts
+++ src/lib/ngx-dynamic-script-loader.config.ts
@@ -16,11 +16,11 @@
  export interface NgxDynamicScript {
    /** An unique name for the script. */
    name: string;
- 
+
    /** The script's URL to fetch. */
    src: string;
  }
- 
+
  /**
   * Interface representing the underlying object used when attempting to load
   * a remote JavaScript module.
@@ -32,18 +32,18 @@
     * Whether the script referred to is loaded.
     */
    loaded: boolean;
- 
+
    /**
     * Whether the script referred to was already fetched from remote server.
     */
    fetched: boolean;
- 
+
    /**
     * Whether the script referred to is loaded.
     */
    module?: Function | object;
  }
- 
+
  /**
   * Options passed to {@link NgxDynamicScriptLoader#loadScript} method.
   *
@@ -67,7 +67,7 @@
     * @default true
     */
    skipError: boolean;
- 
+
    /**
     * Whether to throw an error when loading a {@link NgxDynamicScript} failed.
     * `true` for throwing an error; `false` for skipping throwing the error.
@@ -79,21 +79,21 @@
     * @default true
     */
    skipAbort: boolean;
- 
+
    /**
     * Callback that fires when an error occures while loading the script.
     *
     * @default noop
     */
    onError(error: any): void;
- 
+
    /**
     * Callback that fires when an error occures while loading the script.
     *
     * @default noop
     */
    onAbort(error: any): void;
- 
+
    /**
     * Callback that fires when script reference was loaded successfully.
     *
@@ -101,17 +101,15 @@
     */
    onLoad(script: NgxDynamicScriptLoaded): void;
  }
- 
- function noop(...args: any[]): void {
-   return undefined;
- }
- 
+
+ const noop = (...args: any[]): void => undefined;
+
  /**
   *
   * @publicApi
   */
  export interface NgxDynamicScriptRef extends NgxDynamicScript, Partial<NgxDynamicScriptConfig> {}
- 
+
  export const NGX_DEFAULT_LOAD_SCRIPT_CONFIG: NgxDynamicScriptConfig = {
    async: true,
    skipError: true,
@@ -120,7 +118,7 @@
    onLoad: noop,
    onAbort: noop,
  };
- 
+
  /**
   * Configuration service to
   *
@@ -134,7 +132,7 @@
     * @default true
     */
    async: boolean = NGX_DEFAULT_LOAD_SCRIPT_CONFIG.async;
- 
+
    /**
     * Whether to throw an error when loading a {@link NgxDynamicScript} failed.
     * `true` for throwing an error; `false` for skipping throwing the error.
@@ -146,7 +144,7 @@
     * @default true
     */
    skipError: boolean = NGX_DEFAULT_LOAD_SCRIPT_CONFIG.skipError;
- 
+
    /**
     * Whether to throw an error when loading a {@link NgxDynamicScript} failed.
     * `true` for throwing an error; `false` for skipping throwing the error.
@@ -158,21 +156,21 @@
     * @default true
     */
    skipAbort: boolean = NGX_DEFAULT_LOAD_SCRIPT_CONFIG.skipAbort;
- 
+
    /**
     * Callback that fires when an error occures while loading the script.
     *
     * @default noop
     */
    onError: (reason: any) => void = NGX_DEFAULT_LOAD_SCRIPT_CONFIG.onError;
- 
+
    /**
     * Callback that fires when canceling loading the script.
     *
     * @default noop
     */
    onAbort: (reason: any) => void = NGX_DEFAULT_LOAD_SCRIPT_CONFIG.onAbort;
- 
+
    /**
     * Callback that fires when script reference was loaded successfully.
     *
@@ -180,4 +178,3 @@
     */
    onLoad: (script: NgxDynamicScriptLoaded) => void = NGX_DEFAULT_LOAD_SCRIPT_CONFIG.onLoad;
  }
- 
